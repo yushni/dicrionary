63fc55d90e9a597d1b26555f65bb9f32c04f4772
Bogdan Borysei <borysej90@gmail.com>
2020-06-17 19:01:55 +0300

    Generated API server

---
+++ restapi/configure_dictionary.go
@@ -0,0 +1,91 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"io"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"dictionary/restapi/operations"
+	"dictionary/restapi/operations/words"
+)
+
+//go:generate swagger generate server --target ..\..\dictionary --name Dictionary --spec ..\api\swagger.yaml
+
+func configureFlags(api *operations.DictionaryAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.DictionaryAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.ApplicationJSONProducer = runtime.ProducerFunc(func(w io.Writer, data interface{}) error {
+		return errors.NotImplemented("applicationJson producer has not yet been implemented")
+	})
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.WordsGetWordsHandler == nil {
+		api.WordsGetWordsHandler = words.GetWordsHandlerFunc(func(params words.GetWordsParams) middleware.Responder {
+			return middleware.NotImplemented("operation words.GetWords has not yet been implemented")
+		})
+	}
+	if api.WordsAddWordHandler == nil {
+		api.WordsAddWordHandler = words.AddWordHandlerFunc(func(params words.AddWordParams) middleware.Responder {
+			return middleware.NotImplemented("operation words.AddWord has not yet been implemented")
+		})
+	}
+	if api.WordsDeleteWordHandler == nil {
+		api.WordsDeleteWordHandler = words.DeleteWordHandlerFunc(func(params words.DeleteWordParams) middleware.Responder {
+			return middleware.NotImplemented("operation words.DeleteWord has not yet been implemented")
+		})
+	}
+	if api.WordsGetWordHandler == nil {
+		api.WordsGetWordHandler = words.GetWordHandlerFunc(func(params words.GetWordParams) middleware.Responder {
+			return middleware.NotImplemented("operation words.GetWord has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix"
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
