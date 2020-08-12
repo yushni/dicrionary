f5e6bd214a3845f726da064ac7a7a02c955edf89
borysej90 <borysej90@gmail.com>
2020-08-12 18:35:04 +0300

    Implemented migration function

---
+++ migrate/migrate.go
@@ -0,0 +1,33 @@
+package migrate
+
+import (
+	"errors"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"strings"
+)
+
+func RunMigrate(upOrDown string) error {
+	m, err := migrate.New(
+		"file://migrate/migrations",
+		"postgres://admin:admin@localhost:5432/dictionary",
+	)
+	if err != nil {
+		return err
+	}
+
+	switch strings.ToLower(upOrDown) {
+	case "up":
+		err = m.Up()
+	case "down":
+		err = m.Down()
+	default:
+		return errors.New("unspecified weather upgrade or downgrade migration version")
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
