bde887fd232836fcc7e9eeb50aa716f7abe3cafc
borysej90 <borysej90@gmail.com>
2020-08-21 19:16:13 +0300

    Changed tests for DBConfig

--- migration/migrate_test.go
+++ migration/migrate_test.go
@@ -7,108 +7,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type migrateTest struct {
-	name      string
-	useConfig bool
-	wantError bool
-	downgrade bool
-	errorMsg  string
-	config    DBConfig
-}
-
-func initMigrationTest(t *testing.T, tests []migrateTest, opts ...MigrateOption) {
-
-	// tc means "test case"
-
-}
-
-func TestDBConfig(t *testing.T) {
-	cases := []migrateTest{
-		{
-			name:      "No credentials error",
-			wantError: true,
-			errorMsg:  ErrNoCredentials.Error(),
-		},
-		{
-			name:      "No host and user",
-			wantError: true,
-			errorMsg:  "migration: missing fields: Host, User",
-			useConfig: true,
-			config:    DBConfig{},
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			_, err := NewMigrate()
-
-			if tc.useConfig {
-				_, err = NewMigrate(
-					WithConfig(tc.config),
-				)
-			}
-
-			if tc.downgrade {
-				_, err = NewMigrate(
-					Downgrade(),
-				)
-			}
-
-			if err != nil && tc.wantError {
-				assert.EqualError(t, err, tc.errorMsg)
-			} else if err != nil {
-				failUnexpected(t, err)
-			}
-		})
-	}
-
-	t.Run("Use default port", func(t *testing.T) {
-		cfg := DBConfig{
-			User: "qwe",
-			Host: "qwe",
-		}
-
-		m, err := NewMigrate(
-			WithConfig(cfg),
+func TestNewDBConfig(t *testing.T) {
+	t.Run("Is default port set", func(t *testing.T) {
+		cfg := NewDBConfig(
+			"qwe",
+			"",
+			"",
 		)
-		if err != nil {
-			t.Fatal(err)
-		}
 
-		assert.Equal(t, m.address, fmt.Sprintf("%s:%s", cfg.Host, defaultPostgresPort))
+		assert.Equal(t, fmt.Sprintf("qwe:%s", defaultPostgresPort), cfg.address)
 	})
 
-	t.Run("User creds without password", func(t *testing.T) {
-		cfg := DBConfig{
-			User: "qwe",
-			Host: "qwe",
-		}
-
-		m, err := NewMigrate(
-			WithConfig(cfg),
+	t.Run("Is passed password used", func(t *testing.T) {
+		cfg := NewDBConfig(
+			"",
+			"",
+			"qwe",
+			"asd",
 		)
-		if err != nil {
-			t.Fatal(err)
-		}
 
-		assert.Equal(t, m.userCreds, fmt.Sprintf("%s", cfg.User))
+		assert.Equal(t, "qwe:asd", cfg.userCreds)
 	})
 
-	t.Run("User creds with password", func(t *testing.T) {
-		cfg := DBConfig{
-			User:     "qwe",
-			Password: "qwe",
-			Host:     "qwe",
-		}
-
-		m, err := NewMigrate(
-			WithConfig(cfg),
+	t.Run("Multiple passwords passed", func(t *testing.T) {
+		cfg := NewDBConfig(
+			"",
+			"",
+			"qwe",
+			"1",
+			"2",
+			"3",
 		)
-		if err != nil {
-			t.Fatal(err)
-		}
 
-		assert.Equal(t, m.userCreds, fmt.Sprintf("%s:%s", cfg.User, cfg.Password))
+		assert.Equal(t, "qwe:1", cfg.userCreds)
 	})
 }
 
