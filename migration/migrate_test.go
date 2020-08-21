e5b77fe89d5dbbb45a4071bcf8eb2e0457df2bf6
borysej90 <borysej90@gmail.com>
2020-08-21 21:15:27 +0300

    Fixed tests

--- migration/migrate_test.go
+++ migration/migrate_test.go
@@ -18,32 +18,31 @@ func TestNewDBConfig(t *testing.T) {
 		assert.Equal(t, fmt.Sprintf("qwe:%s", defaultPostgresPort), cfg.address)
 	})
 
+	t.Run("Multiple passwords passed", func(t *testing.T) {
+		cfg := NewDBConfig(
+			"qwe",
+			"",
+			"",
+		)
+		cfg.SetPort("asd")
+
+		assert.Equal(t, "qwe:asd", cfg.address)
+	})
+
 	t.Run("Is passed password used", func(t *testing.T) {
 		cfg := NewDBConfig(
 			"",
 			"",
 			"qwe",
-			"asd",
 		)
+		cfg.SetPassword("asd")
 
 		assert.Equal(t, "qwe:asd", cfg.userCreds)
 	})
-
-	t.Run("Multiple passwords passed", func(t *testing.T) {
-		cfg := NewDBConfig(
-			"",
-			"",
-			"qwe",
-			"1",
-			"2",
-			"3",
-		)
-
-		assert.Equal(t, "qwe:1", cfg.userCreds)
-	})
 }
 
-func failUnexpected(t *testing.T, err error) {
+// Temporary unused
+/*func failUnexpected(t *testing.T, err error) {
 	t.Logf("Expected no error but got: %s", err.Error())
 	t.FailNow()
-}
+}*/
